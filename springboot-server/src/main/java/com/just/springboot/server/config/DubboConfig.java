8f45037a25c185c46ed82f16374831b2285e0913
李朋 <lipeng45@58.com>
2020-08-20 19:47:52 +0800

    init

---
+++ springboot-server/src/main/java/com/just/springboot/server/config/DubboConfig.java
@@ -0,0 +1,9 @@
+package com.just.springboot.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({ "classpath:dubbo/*.xml" })
+public class DubboConfig {
+}
