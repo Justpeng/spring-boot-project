3b2744205c6243a98626aaac3a064431c5f35ca3
李朋 <lipeng45@58.com>
2020-10-13 16:19:03 +0800

    java dubbo adaptive

--- dubbo-consumer/src/test/java/com/just/test/DubboSPITest.java
+++ dubbo-consumer/src/test/java/com/just/test/DubboSPITest.java
@@ -1,5 +1,6 @@
 package com.just.test;
 
+import com.just.dubbo.server.spi.AdaptiveOrder;
 import com.just.dubbo.server.spi.Order;
 import org.apache.dubbo.common.extension.ExtensionLoader;
 import org.junit.Test;
@@ -20,10 +21,17 @@ public class DubboSPITest {
 
     @Test
     public void test02() {
-        ExtensionLoader<Order> loader = ExtensionLoader.getExtensionLoader(Order.class);
-
-//        Order order = loader.getExtension(null);
-        Order order = loader.getAdaptiveExtension();
-        order.pay();
+        ExtensionLoader<Order> extensionLoader = ExtensionLoader.getExtensionLoader(Order.class);
+        Order adaptiveExtension = extensionLoader.getAdaptiveExtension();
+        adaptiveExtension.pay();
     }
+
+    @Test
+    public void test03() {
+        ExtensionLoader<Order> extensionLoader = ExtensionLoader.getExtensionLoader(Order.class);
+        Order adaptive = extensionLoader.getAdaptiveExtension();
+        ((AdaptiveOrder)adaptive).setAdaptiveName("alipay");
+        adaptive.pay();
+    }
+
 }
